1ae496c59478a113b96371ac5cc2636113e0fd92
100293231 <lirihong@huaqin.com>
2024-06-27 15:07:24 +0800

    merge测试

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("niugit");
+        System.out.println("das");
     }
 }
+git add jgi
