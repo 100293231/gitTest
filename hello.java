e7af46160b9b4d8a18d112c309eebfc72770e616
100293231 <lirihong@huaqin.com>
2024-06-27 15:07:24 +0800

    append with hello.java

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("niugit");
+        System.out.println("das");
     }
 }
